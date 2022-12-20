345678647ce467faf21ecc7142f56ed2bd0dda92
Antek93 <antonio.savino1993@gmail.com>
2022-12-20 16:50:22 +0100

    style secondo blocco

--- js/scripts.js
+++ js/scripts.js
@@ -40,18 +40,34 @@ sottometti.addEventListener('click',
                          
 );
 
+// MILESTONE 2 - STAMPA SU HTML
 
-// const age = parseInt(prompt('Inserisci tua età'));
-// console.log("Età utente:", age);
 
-// if (age >= 18 && age <= 65) {
-//     console.log("Costo biglietto standard:", ((ticket_fair * 100) / 100) .toFixed(2) + "€");
 
-// } else if (age < 18) {
-//     let ticket_fair = (distance * ticket_unit) - ((distance * ticket_unit) * 20 / 100 );
-//     console.log("Costo biglietto sconto minorenni:", ((ticket_fair * 100) / 100) .toFixed(2) + "€");
+sottometti.addEventListener('click', 
+                         
+    function() {
 
-// } else if (age >= 30 && age <= 34){
-//     let ticket_fair = (distance * ticket_unit) - ((distance * ticket_unit) * 100 / 100 );
-//     console.log("Costo biglietto sconto anziani:", ((ticket_fair * 100) / 100) .toFixed(2) + "€");
-// }
+        console.log('Cliccato su submitButton');
+        console.log("L'utente si chiama:", identità.value);
+        console.log("L'utente percorrerà", kilo.value + "km");
+        console.log("L'utente appartiene al range", choice.value);
+        
+        if(choice.value == "anni 18-65") {
+
+            let ticketFair = ((ticketUnit*kilo.value) * 1);
+            console.log("Tariffa standard:", ticketFair .toFixed(2) + "€")
+
+        } else if(choice.value == "anni -18") {
+            let ticketFair = ((ticketUnit*kilo.value) * 0.8);
+            console.log("Tariffa sconto minori", ticketFair .toFixed(2) + "€")
+
+        } else {
+            let ticketFair = ((ticketUnit*kilo.value) * 0.6 );
+            console.log("Tariffa sconto over 65", ticketFair .toFixed(2) + "€")
+        }
+
+    }
+                         
+);
+
