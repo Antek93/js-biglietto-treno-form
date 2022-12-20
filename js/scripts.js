6fc28e5b3a16e3cba1edff4595c0e3a298728f96
Antek93 <antonio.savino1993@gmail.com>
2022-12-20 17:26:22 +0100

    aggiunto menu a scomparsa e variabili IF

--- js/scripts.js
+++ js/scripts.js
@@ -42,31 +42,33 @@ sottometti.addEventListener('click',
 
 // MILESTONE 2 - STAMPA SU HTML
 
+let scomparsa = document.getElementById("hiding");
 
 
 sottometti.addEventListener('click', 
                          
     function() {
-
-        console.log('Cliccato su submitButton');
-        console.log("L'utente si chiama:", identità.value);
-        console.log("L'utente percorrerà", kilo.value + "km");
-        console.log("L'utente appartiene al range", choice.value);
         
+        scomparsa.style.display = "block";
+        document.getElementById("utente").innerHTML = identità.value
+
         if(choice.value == "anni 18-65") {
 
             let ticketFair = ((ticketUnit*kilo.value) * 1);
-            console.log("Tariffa standard:", ticketFair .toFixed(2) + "€")
+            document.getElementById("tipo-biglietto").innerHTML = "Biglietto Standard";
+            document.getElementById("ticket-price").innerHTML = (ticketFair .toFixed(2) + "€");
 
         } else if(choice.value == "anni -18") {
             let ticketFair = ((ticketUnit*kilo.value) * 0.8);
-            console.log("Tariffa sconto minori", ticketFair .toFixed(2) + "€")
+            document.getElementById("tipo-biglietto").innerHTML = "Biglietto -20%";
+            document.getElementById("ticket-price").innerHTML = (ticketFair .toFixed(2) + "€");
+
 
         } else {
             let ticketFair = ((ticketUnit*kilo.value) * 0.6 );
-            console.log("Tariffa sconto over 65", ticketFair .toFixed(2) + "€")
+            document.getElementById("tipo-biglietto").innerHTML = "Biglietto -40%";
+            document.getElementById("ticket-price").innerHTML = (ticketFair .toFixed(2) + "€");
         }
-
     }
                          
 );
